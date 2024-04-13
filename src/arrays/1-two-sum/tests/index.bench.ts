a0ada1bec9821fc1064e55b0573ac7afd1f5ce52
Gabb-c <gdgabrieldacunha71@gmail.com>
2024-04-13 02:24:12 -0300

    feat: add github ci and expand tests

--- src/arrays/1-two-sum/tests/index.bench.ts
+++ src/arrays/1-two-sum/tests/index.bench.ts
@@ -3,10 +3,13 @@ import { bench, describe, expect } from "vitest";
 import { twoSum } from "..";
 import { TEST_DATA } from "./test-data";
 
-describe("Two Sum", () => {
-  for (const { input, expected } of TEST_DATA) {
-    bench(`should find the indices of two numbers that add up to ${input.target}`, () => {
-      expect(twoSum(input.nums.slice(), input.target)).toEqual(expected);
+describe("Two Sum (benchmark)", () => {
+  for (const {
+    input: { nums, target },
+    expected,
+  } of TEST_DATA) {
+    bench(`should find the indices of two numbers that add up to ${target}`, () => {
+      expect(twoSum(nums, target)).toEqual(expected);
     });
   }
 });
--- src/arrays/1-two-sum/tests/index.spec.ts
+++ src/arrays/1-two-sum/tests/index.spec.ts
@@ -4,9 +4,16 @@ import { twoSum } from "..";
 import { TEST_DATA } from "./test-data";
 
 describe("Two Sum", () => {
-  for (const { input, expected } of TEST_DATA) {
-    it(`should find the indices of two numbers that add up to ${input.target}`, () => {
-      expect(twoSum(input.nums.slice(), input.target)).toEqual(expected);
+  for (const {
+    input: { nums, target },
+    expected,
+  } of TEST_DATA) {
+    it(`should find the indices of two numbers that add up to ${target}`, () => {
+      // Validate inputs
+      expect(nums).toBeDefined();
+      expect(target).toBeDefined();
+      // Validate output
+      expect(twoSum(nums, target)).toEqual(expected);
     });
   }
 });
