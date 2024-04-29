7ed73cb1c55bcb8421b33b4f8b063c090747eb4d
Gabb-c <gdgabrieldacunha71@gmail.com>
2024-04-29 10:02:08 -0300

    chore(deps): update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  esbuild: { target: "ESNext" },
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
